0284fe4f8aedb156a9e7b02a87fe951bd2f2a9ab
Rafey <mfarooqi.bese21seecs@seecs.edu.pk>
2025-08-27 12:50:20 +0500

    Update GSVCitiesDataset.py

--- dataloaders/GSVCitiesDataset.py
+++ dataloaders/GSVCitiesDataset.py
@@ -14,7 +14,7 @@ default_transform = T.Compose([
 ])
 
 # NOTE: Hard coded path to dataset folder 
-BASE_PATH = '../data/GSVCities/'
+BASE_PATH = '/root/.cache/kagglehub/datasets/amaralibey/gsv-cities/versions/1'
 
 if not Path(BASE_PATH).exists():
     raise FileNotFoundError(
